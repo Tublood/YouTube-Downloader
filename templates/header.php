192a03e70c4dfc8158112c3b09a4733518e01a64
tunguyen <tunguyen@brodev.com>
2019-05-26 12:04:52 +0700

    Seo ready

--- templates/header.php
+++ templates/header.php
@@ -18,27 +18,27 @@ $currentURL = $_SERVER['REQUEST_SCHEME'] . "://" .$_SERVER['SERVER_NAME'] . $add
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta charset="utf-8">
-	<title><?php echo $this->get('title', 'Youtube Downloader'); ?></title>
-	<meta name="keywords" content="Video downloader, download youtube, video download, youtube video, youtube downloader, download youtube FLV, download youtube MP4, download youtube 3GP, php video downloader"/>
-	<meta name="description" content="Video downloader, download youtube, video download, youtube video, youtube downloader, download youtube FLV, download youtube MP4, download youtube 3GP, php video downloader"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<link href="<?php echo $bbPath?>css/bootstrap.min.css" rel="stylesheet" media="screen">
-	<link href="<?php echo $bbPath?>css/custom.css" rel="stylesheet">
-	<style type="text/css">
-		#info {
-			padding: 0 0 0 130px;
-			position: relative;
-			height: 100px;
-		}
-		#info img {
-			left: 0;
-			position: absolute;
-			top: 0;
-			width: 120px;
-			height: 90px
-		}
-	</style>
+    <meta charset="utf-8">
+    <title><?php echo $this->get('title', 'Youtube Downloader'); ?></title>
+    <meta name="keywords" content="Video downloader, download youtube, video download, youtube video, youtube downloader, download youtube FLV, download youtube MP4, download youtube 3GP, php video downloader"/>
+    <meta name="description" content="Video downloader, download youtube, video download, youtube video, youtube downloader, download youtube FLV, download youtube MP4, download youtube 3GP, php video downloader"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="<?php echo $bbPath?>css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="<?php echo $bbPath?>css/custom.css" rel="stylesheet">
+    <style type="text/css">
+        #info {
+            padding: 0 0 0 130px;
+            position: relative;
+            height: 100px;
+        }
+        #info img {
+            left: 0;
+            position: absolute;
+            top: 0;
+            width: 120px;
+            height: 90px
+        }
+    </style>
 
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140690718-1"></script>
     <script>
@@ -70,22 +70,22 @@ $currentURL = $_SERVER['REQUEST_SCHEME'] . "://" .$_SERVER['SERVER_NAME'] . $add
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top">
-	<div class="container">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="http://youtubedownloadervideos.com">Youtube Video Downloader</a>
-		</div>
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="http://youtubedownloadervideos.com">Youtube Video Downloader</a>
+        </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li><a href="http://youtubedownloadervideos.com">Home</a></li>
-                <li><a href="<?php echo $bbPath?>convert-youtube-to-mp3/index.php">Convert Youtube to mp3</a></li>
+                <li><a href="<?php echo $bbPath?>convert-youtube-to-mp3/">Convert Youtube to mp3</a></li>
             </ul>
         </div><!--/.nav-collapse -->
-	</div>
+    </div>
 </nav>
 <div class="container">
--- templates/mp3index.php
+++ templates/mp3index.php
@@ -1,66 +1,68 @@
-<?php echo $this->inc('header.php', ['title' => 'Youtube MP3 Converter']); ?>
-	<div class="well">
-		<form class="" method="get" id="download" action="getmp3.php">
-            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <!-- ad21 -->
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 data-ad-client="ca-pub-4631273244511276"
-                 data-ad-slot="8858021838"
-                 data-ad-format="link"
-                 data-full-width-responsive="true"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-			<h1 class="form-download-heading">Convert Youtube To MP3</h1>
-				<div class="input-group">
-				  <input type="text" name="videoid" id="videoid" class="form-control input-lg" placeholder="YouTube Link or VideoID" autofocus>
-				  <span class="input-group-btn">
+<head>
+    <?php echo $this->inc('header.php', ['title' => 'Youtube MP3 Converter - Youtube2mp3']); ?>
+    <meta name="description" content="Youtube2mp3. Convert and download youtube videos to mp3 audio files for free.There is no registration or software needed"/>
+    <div class="well"><head>
+            <form class="" method="get" id="download" action="getmp3.php">
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- ad21 -->
+                <ins class="adsbygoogle"
+                     style="display:block"
+                     data-ad-client="ca-pub-4631273244511276"
+                     data-ad-slot="8858021838"
+                     data-ad-format="link"
+                     data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+                <head>
+                    <h1 class="form-download-heading">Convert Youtube To MP3</h1>
+                    <div class="input-group">
+                        <input type="text" name="videoid" id="videoid" class="form-control input-lg" placeholder="YouTube Link or VideoID" autofocus>
+                        <span class="input-group-btn">
 					<input class="btn btn-primary btn-lg" type="submit" name="type" id="type" value="Convert" />
 				  </span>
-				</div><!-- /input-group -->
-			<div class="video-info">
-				<p>Valid inputs are YouTube links or VideoIDs:</p>
-					<ul>
-						<li>youtube.com/watch?v=...</li>
-						<li>youtu.be/...</li>
-						<li>youtube.com/embed/...</li>
-						<li>youtube-nocookie.com/embed/...</li>
-						<li>youtube.com/watch?feature=player_embedded&v=...</li>
-					</ul>
-			</div>
-            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <!-- ad14 -->
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 data-ad-client="ca-pub-4631273244511276"
-                 data-ad-slot="5938473435"
-                 data-ad-format="auto"
-                 data-full-width-responsive="true"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-            <h2>Video Downloader Features</h2>
-            <div class="content">
+                    </div><!-- /input-group -->
+                    <div class="video-info">
+                        <p>Valid inputs are YouTube links or VideoIDs:</p>
+                        <ul>
+                            <li>youtube.com/watch?v=...</li>
+                            <li>youtu.be/...</li>
+                            <li>youtube.com/embed/...</li>
+                            <li>youtube-nocookie.com/embed/...</li>
+                            <li>youtube.com/watch?feature=player_embedded&v=...</li>
+                        </ul>
+                    </div>
+                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <!-- ad14 -->
+                    <ins class="adsbygoogle"
+                         style="display:block"
+                         data-ad-client="ca-pub-4631273244511276"
+                         data-ad-slot="5938473435"
+                         data-ad-format="auto"
+                         data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                    <h2>Video Downloader Features</h2>
+                    <div class="content">
 
-                Our video downloader is completely free and easy to use without requiring you to register for an account or download any software. Moreover, it offers the following features:
-                <ul>
-                    <li>Download videos in HD, SD, and many other available resolutions</li>
-                    <li>Unlimited downloads without any restrictions</li>
-                    <li>Free to use</li>
-                    <li>Fast downloads and conversions with no rate limiting</li>
-                    <li>Works on any device, whether it be Windows, macOS, iOS, Linux, or Android</li>
-                    <li>100% safe &amp; secure</li>
-                </ul>
-            </div>
-		<!-- @TODO: Prepend the base URI -->
-		<?php
-		if ( $this->get('showBrowserExtensions') === true )
-		{
-			echo '<center><a href="ytdl.user.js" class="btn btn-sm btn-success" title="Install chrome extension to view a \'Download\' link to this application on Youtube video pages."> Install Chrome Extension </a></center>';
-		}
-		?>
-		<div class="clearfix"></div>
-		</form>
-	</div>
-<?php echo $this->inc('footer.php'); ?>
+                        Our video downloader is completely free and easy to use without requiring you to register for an account or download any software. Moreover, it offers the following features:
+                        <ul>
+                            <li>Unlimited downloads without any restrictions</li>
+                            <li>Free to use</li>
+                            <li>Fast downloads and conversions with no rate limiting</li>
+                            <li>Works on any device, whether it be Windows, macOS, iOS, Linux, or Android</li>
+                            <li>100% safe &amp; secure</li>
+                        </ul>
+                    </div>
+                    <!-- @TODO: Prepend the base URI -->
+                    <?php
+                    if ( $this->get('showBrowserExtensions') === true )
+                    {
+                        echo '<center><a href="ytdl.user.js" class="btn btn-sm btn-success" title="Install chrome extension to view a \'Download\' link to this application on Youtube video pages."> Install Chrome Extension </a></center>';
+                    }
+                    ?>
+                    <div class="clearfix"></div>
+            </form>
+    </div>
+    <?php echo $this->inc('footer.php'); ?>
